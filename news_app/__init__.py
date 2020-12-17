9c78d01a5c7daa9218ef1873b9666e602259f8bb
320028626 <vinay.r@philips.com>
2020-12-17 17:23:20 +0530

    Register the blueprints to app. Also added a method to create_app with given configurations.

--- news_app/__init__.py
+++ news_app/__init__.py
@@ -4,11 +4,22 @@ from news_app.settings import Configurations
 from flask_bcrypt import Bcrypt
 
 config = Configurations()
-app = Flask(__name__)
-app.config['SECRET_KEY'] = config.SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+db = SQLAlchemy()
+bcrypt=Bcrypt()
 
-db = SQLAlchemy(app)
-bcrypt=Bcrypt(app)
+def create_app(config=config):
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = config.SECRET_KEY
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+    app.config['NEWS_API_KEY'] = config.NEWS_API_KEY
 
-from news_app import routes
+    db.init_app(app)
+    bcrypt.init_app(app)
+
+    from news_app.users.routes import users
+    from news_app.news.routes import news
+
+    app.register_blueprint(users)
+    app.register_blueprint(news)
+
+    return app
